c0875014adb7d8f4ff6d854928b0afacdda5acf9
Nathan Montoya <montoya.nlm@gmail.com>
2026-06-14 23:05:49 -0600

    PySLM block support generation

---
+++ SupportGen/PySLMBSG.py
@@ -0,0 +1,27 @@
+import trimesh
+from pyslm.core import Part
+from pyslm.support import BlockSupportGenerator, getOverhangMesh
+
+mesh = trimesh.load_mesh("C:\\Users\\monto\\Downloads\\Xrot-Bone-Remesh.stl")
+
+part = Part("part")
+part.setGeometry(mesh)
+overhead_angle = 31.5
+
+
+support_generator = BlockSupportGenerator()
+support_structures = support_generator.identifySupportRegions(part, overhangAngle=overhead_angle)
+
+build = [part]
+
+for i,support in enumerate(support_structures):
+    support_mesh = support.geometry()
+
+    support_part = Part(f"support_{i}")
+    support_part.setGeometry(support_mesh)
+
+    build.append(support_part)
+
+full = trimesh.util.concatenate([spart.geometry for spart in build])
+
+full.export("C:\\Users\\monto\\Downloads\\supported_model.stl")
